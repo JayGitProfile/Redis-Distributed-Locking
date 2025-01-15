a7fdbc467f499c292a723ff05bcb287cccb20bdd
JayGitProfile <jayakanth008@gmail.com>
2025-01-14 21:57:04 -0600

    initial commit

---
+++ src/main/java/com/redis/locking/RedisDistributedLockingApplication.java
@@ -0,0 +1,13 @@
+package com.redis.locking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisDistributedLockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDistributedLockingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redis/locking/RedisDistributedLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.locking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDistributedLockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
