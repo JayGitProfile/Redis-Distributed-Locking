02b25ca2ba0ae62c2564485b304d13d04e8a4f87
JayGitProfile <jayakanth008@gmail.com>
2025-01-14 22:24:41 -0600

    project setup

--- src/test/java/com/redis/locking/RedisDistributedLockingApplicationTests.java
+++ src/test/java/com/redis/locking/RedisDistributedLockingApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RedisDistributedLockingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
